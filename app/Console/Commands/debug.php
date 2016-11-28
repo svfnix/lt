c8cf6e6c9b2a0753952f97bea65046ba35d9c482
saeed <psproot@gmail.com>
2016-11-28 15:06:59 +0330

    submit

--- app/Console/Commands/debug.php
+++ app/Console/Commands/debug.php
@@ -10,14 +10,6 @@ class debug extends Command
     protected $signature = 'tg:debug';
     protected $description = 'Debug tool';
 
-    function file_url($file){
-        return 'https://api.telegram.org/file/bot'.config('telegram.bot_token').'/'.$file;
-    }
-
-    function addSignature($message){
-        return "{$message}\n\n💟 @telegfa";
-    }
-
     public function handle()
     {
         $telegram = new Api();
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\hook::class,
+        Commands\hookoff::class,
         Commands\debug::class
     ];
 
